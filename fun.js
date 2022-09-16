7dc0af122df43cad141dd0b8b5b15df33f6bc3f8
github_username <you@domain.com>
2022-09-16 10:43:01 +0600

    first commit

---
+++ fun.js
@@ -0,0 +1,16 @@
+// function addTo(num1, num2){
+//     var num=num1+num2;
+//    return(num)
+// }
+// var number = addTo(2,3);
+// console.log(number)
+// odd number system
+function oddNum(num){
+    if(num % 2==0){
+       console.log('it is oddNum');
+    }
+    else{console.log('it is not oddNum')}
+}
+
+//var sum =oddNum(45);
+console.log(oddNum(4));
