e070c4ba92783f6ae5718a0ff404bfa237fa4b96
github_username <you@domain.com>
2022-09-16 12:18:37 +0600

    factorial number

--- fun.js
+++ fun.js
@@ -4,24 +4,41 @@
 // }
 // var number = addTo(2,3);
 // console.log(number)
-// odd number system
-function oddNum(num){
-    if(num % 2==0){
-      return('it is oddNum');
+
+// 2 odd number system
+
+// function oddNum(num){
+//     if(num % 2==0){
+//       return('it is oddNum');
+//     }
+//     else{return('it is not oddNum')}
+// }
+
+// var sum =oddNum(45);
+// console.log(sum);
+
+//  3 switch case
+
+// switch ('shop'){
+// case('shop'):
+// console.log('it is data');
+// break;
+// case('market'):
+// console.log('It is market');
+// break;
+// default:
+//     console.log('data not found!!')
+// }
+
+// 4 factorial no
+
+function fac(n){
+    let result = 1;
+    let i = n;
+    while(i>=1){
+        result = result * i;
+        i--;    
     }
-    else{return('it is not oddNum')}
+    return result;
 }
-
-var sum =oddNum(45);
-console.log(sum);
-
-switch ('shop'){
-case('shop'):
-console.log('it is data');
-break;
-case('market'):
-console.log('It is market');
-break;
-default:
-    console.log('data not found!!')
-}
+console.log(fac(4));
